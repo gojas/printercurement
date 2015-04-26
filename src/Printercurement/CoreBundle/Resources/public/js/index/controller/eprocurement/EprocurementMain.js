119a8cfdab08f9d38607431748c89926875feee9
gojas <gojas90@gmail.com>
2015-04-26 15:46:04 +0200

    cart widget added

--- src/Printercurement/CoreBundle/Resources/public/js/index/controller/eprocurement/EprocurementMain.js
+++ src/Printercurement/CoreBundle/Resources/public/js/index/controller/eprocurement/EprocurementMain.js
@@ -4,10 +4,17 @@ indexApp.controller('EprocurementMain', function EprocurementMain($scope) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
 
+    //will be taken from factory
     $scope.highchartData = [
         ['ideas1', 1],
         ['ideas2', 8],
         ['ideas3', 5]
+    ];
+
+    //will be taken from factory
+    $scope.cartData = [
+        {name: 'bla bla1', catalogUrl: 'bl-aaa', articleUrl: 'bla-bla1', price: 13, currency: 'EUR'},
+        {name: 'bla bla2', catalogUrl: 'bl-aaa', articleUrl: 'bla-bla2', price: 12.5, currency: 'EUR'}
     ];
 
 });
---
+++ src/Printercurement/CoreBundle/Resources/public/js/index/directive/eprocurement/CartWidget.js
@@ -0,0 +1,56 @@
+indexApp.directive('cartWidget', function ($compile){
+
+    var createCartTemplate = function(items){
+
+        template = '<div class="cart-widget-items">';
+        angular.forEach(items, function(item) {
+            template +=
+            '<div>' +
+                '<a href="/eprocurement/'+item.catalogUrl+'/'+item.articleUrl+'">'+item.name+'</a>' +
+            '</div>';
+        });
+
+        template += '</div>';
+
+        return template;
+    };
+
+    return {
+        restrict: 'A',
+        scope: {
+            items: '='
+        },
+        controller: function ($scope, $element, $attrs) {
+            //
+        },
+        template: 'Cart - hover over me (da prostis)',
+        link: function (scope, element) {
+
+            cartItems = scope.items;
+
+            element.bind('mouseenter', function() {
+                element.html(createCartTemplate(cartItems)).show();
+
+                /**
+                 * do not move out of this listener
+                 *
+                 * let AngularJs knows that there are some changes on the scope
+                */
+                scope.$apply();
+            });
+
+            element.bind('mouseleave', function() {
+                element.html('Cart - hover over me (da prostis)').show();
+
+                /**
+                 * do not move out of this listener
+                 *
+                 * let AngularJs knows that there are some changes on the scope
+                 */
+                scope.$apply();
+            });
+
+            $compile(element.contents())(scope);
+        }
+    }
+});
