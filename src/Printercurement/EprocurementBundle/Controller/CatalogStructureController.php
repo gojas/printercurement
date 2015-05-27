be093d3baa142b3321ec2bf460ca6977572842b8
gojas <gojas90@gmail.com>
2015-05-27 22:34:08 +0200

    prepare tree node view

--- src/Printercurement/EprocurementBundle/Controller/CatalogStructureController.php
+++ src/Printercurement/EprocurementBundle/Controller/CatalogStructureController.php
@@ -37,8 +37,22 @@ class CatalogStructureController extends Controller
             array('parent_node_id' => $nodeId)
         );
 
+        $nodes = array();
+        foreach($catalogsStructure as $structure){
+            $prepareStructure = array();
+            $prepareStructure['node_id'] = $structure->getNodeId();
+            $prepareStructure['parent_node_id'] = $structure->getParentNodeId();
+            $prepareStructure['catalog'] = $structure->getCatalog();
+
+            foreach($structure->getNodes() as $node){
+                $prepareStructure['child_nodes'][] = $node;
+            }
+
+            $nodes[] = $prepareStructure;
+        }
+
         return $this->getRenderer()
-            ->assign('catalogsStructure', $catalogsStructure)
+            ->assign('catalogsStructure', $nodes)
             ->render();
     }
 }
--- src/Printercurement/EprocurementBundle/Resources/public/js/controller/CatalogStructure.js
+++ src/Printercurement/EprocurementBundle/Resources/public/js/controller/CatalogStructure.js
@@ -5,6 +5,18 @@ eprocurementApp.controller('CatalogStructure', ['$scope', 'CatalogStructure', fu
 
     var nodeId = 0;
 
+    $('ul.nav-left-ml').toggle();
+    $('label.nav-toggle span').click(function () {
+        $(this).parent().parent().children('ul.nav-left-ml').toggle(300);
+        var cs = $(this).attr("class");
+        if(cs == 'nav-toggle-icon glyphicon glyphicon-chevron-right') {
+            $(this).removeClass('glyphicon-chevron-right').addClass('glyphicon-chevron-down');
+        }
+        if(cs == 'nav-toggle-icon glyphicon glyphicon-chevron-down') {
+            $(this).removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-right');
+        }
+    });
+
     $scope.catalogsStructure = CatalogStructure.getCatalogsStructure(nodeId).success(function(data) {
         $scope.catalogsStructure = data.catalogsStructure;
     });
